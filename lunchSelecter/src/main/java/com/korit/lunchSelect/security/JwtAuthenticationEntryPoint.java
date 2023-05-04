964030fe555da7aabd5a169e491b6680ae0b1e4f
cifula <lidel95@gmail.com>
2023-05-04 15:14:57 +0900

    Fix: 비밀번호 입력 오류 Response 수정

--- lunchSelecter/src/main/java/com/korit/lunchSelect/security/JwtAuthenticationEntryPoint.java
+++ lunchSelecter/src/main/java/com/korit/lunchSelect/security/JwtAuthenticationEntryPoint.java
@@ -9,12 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.korit.lunchSelect.dto.common.ErrorResponseDto;
+import com.korit.lunchSelect.exception.CustomException;
+import com.korit.lunchSelect.exception.ErrorMap;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -23,11 +27,16 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
 		
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
 		response.setStatus(HttpStatus.UNAUTHORIZED.value());
 		
 		ErrorResponseDto<?> errorResponseDto = 
 				new ErrorResponseDto<AuthenticationException>("", authException);
+		
+		if(authException.getClass() == BadCredentialsException.class || authException.getClass() == InternalAuthenticationServiceException.class) {
+			errorResponseDto = new ErrorResponseDto<>("로그인 실패", ErrorMap.builder().put("email", "사용자 정보를 확인하세요").build());
+		}
+		
 		ObjectMapper objectMapper = new ObjectMapper();
 		String responseJson = objectMapper.writeValueAsString(errorResponseDto);
 		
