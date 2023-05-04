da529098b0f4425cd2bcba59ba7c01fd587b415d
dnr1112 <dnr1112@naver.com>
2023-05-04 15:43:56 +0900

    Init: kakaoAPI - react버전으로 변경

--- front_lunch_selecter/src/Kakao/MapTest.js
+++ front_lunch_selecter/src/Kakao/MapTest.js
@@ -18,6 +18,32 @@ const MapTest = () => {
             level: 3
         };
         const map = new kakao.maps.Map(container, options);
+
+        const ps = new kakao.maps.services.Places(map);
+
+        ps.categorySearch('FD6', placesSearchCB, {useMapBounds: true});
+
+        const placesSearchCB = (data, status, pagination) => {
+            if(status === kakao.maps.services.Status.OK){
+                for(let i = 0; i < data.length; i++){
+                    displayMarker(data[i]);
+                }
+            }
+        }
+
+        const infowindow = new kakao.maps.infoWindow({zIndex:1});
+
+        const displayMarker = (place) => {
+            const marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(place.y, place.x)
+            });
+
+            kakao.maps.event.addListener(marker, 'click', function(){
+                infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+                infowindow.open(map, marker);
+            })
+        }
     }, [])
     
     // useEffect(() => {
