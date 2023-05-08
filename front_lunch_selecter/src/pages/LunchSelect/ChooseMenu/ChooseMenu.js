c98198c1f9d20ace9d2e52a5b27fb41ed508a6a7
kjy3408 <wodud2312@naver.com>
2023-05-08 17:50:56 +0900

    feat : LunchSelect 룰렛 만드는 중

---
+++ front_lunch_selecter/src/pages/LunchSelect/ChooseMenu/ChooseMenu.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+
+const ab = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    justify-content: space-between;
+
+    
+`;
+
+const aaa = css`
+    display: flex;
+    margin-bottom: 200px;
+    margin-top: 200px;
+
+`;
+
+const dd = css`
+    font-size: 100px;
+    margin-bottom: 200px;
+    margin-top: 200px;
+ 
+`;
+
+const cc = css`
+    font-size: 60px;
+    margin: 100px;
+`;
+
+
+
+const ChooseMenu = () => {
+    const location = useLocation();
+    let todayLunch = null;
+    
+    const returnButtonHandle = () => {
+        
+    }
+
+    if (location.state && location.state.value !== null) {
+        todayLunch = location.state.value;
+    }
+
+    return (
+        <div css={ab}>
+            <div css={dd}>{todayLunch}</div>
+            <div css={aaa}>
+                <Link to="/lunchselect"><button css={cc} onClick={returnButtonHandle}>다시돌려~</button></Link>
+                <Link><button css={cc} >음식점 자세히 보기</button></Link>
+            </div>
+        </div>
+    );
+};
+
+export default ChooseMenu;
