adb2ba9b8b3f463b20bcc1d7d3cdc3f36446e8b7
kjy3408 <wodud2312@naver.com>
2023-05-17 14:06:08 +0900

    feat: 룰렛 재구성중

--- front_lunch_selecter/src/pages/LunchSelect/LunchSelect.js
+++ front_lunch_selecter/src/pages/LunchSelect/LunchSelect.js
@@ -1,37 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import axios from 'axios';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import * as s from './style';
 
-import LocalCategory from './LocalCategory/LocalCategory';
 import Category from './category/Category';
 
 
 const selectLocation = (isOpen) => css`
-    /* position: absolute;
-    top: 5%;
-    right: 50%; */
     display: ${isOpen ? "flex" : "none"};
-    /* width: 180px;
-    height: 200px;
-    max-height: 100px;
-    background-color: white;
-    overflow-y: scroll; */
-
 `;
-
-
-
-
 
 const LunchSelect = () => {
     const navigate = useNavigate();
 
-    const [isInvited, setIsInvited] = useState(false);
-    const [inviteCode, setInviteCode] = useState('');
     const [ position, setPosition ] = useState();
     const [ isOpen, setIsOpen ] = useState(false);
     const [ startButtonClickState, setStartButtonClickState ] = useState(false);
@@ -41,38 +25,36 @@ const LunchSelect = () => {
         lng: null
     });
     const [ slotValue, setSlotValue ] = useState([]) 
-    const [todayLunch, setTodayLunch] = useState("");
+    const [todayLunch, setTodayLunch] = useState("오늘의 점심은?");
     const [ address, setAddress ] = useState("");
-
+    const [isSpinning, setIsSpinning] = useState(false);
+    const intervalRef = useRef(null);
+   
     const getLocation = () => {
-        let lat, lng;
-
-        if(navigator.geolocation) {
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
-                function(position) {
-                    lat = 35.152418;
-                    lng = 129.060043;
-                    setGeolocation((geolocation) => {
-                        return {
-                            ...geolocation,
-                            lat,
-                            lng,
-                        };
-                    });
-                    setLocationIsLoading(false);
-                    
-                },
-                function(error) {
-                    console.error(error);
-                },
-                {
-                    enableHighAccuracy: false,
-                    maximumAge: 0,
-                    timeout: Infinity,
-                }
+              function(position) {
+                const { latitude, longitude } = position.coords;
+                setGeolocation((geolocation) => {
+                  return {
+                    ...geolocation,
+                    lat: latitude,
+                    lng: longitude,
+                  };
+                });
+                setLocationIsLoading(false);
+              },
+              function(error) {
+                console.error(error);
+              },
+              {
+                enableHighAccuracy: false,
+                maximumAge: 0,
+                timeout: Infinity,
+              }
             );
-        }
-
+          }
+          
         if(console.error === null) {
             alert('위치 설정을 허용해주세요');
             return;
@@ -90,9 +72,10 @@ const LunchSelect = () => {
           }
         };
         const response = await axios.get("http://localhost:8080/lunch/select", option);
-        console.log(response.data)
-        const names = response.data.map(store => store.name);
+        console.log("현재위치: " + geolocation.lat, geolocation.lng)
+        const names = await response.data.map(store => store.name);
         setSlotValue(names);
+        console.log("names: " + names)
         return response;
       }, {
         enabled: startButtonClickState && !locationIsLoading,
@@ -104,55 +87,57 @@ const LunchSelect = () => {
       });
       
 
-  
-    const [isSpinning, setIsSpinning] = useState(false);
-    const intervalRef = useRef(null);
-
     const handleStart = () => {
         getLocation();
         setStartButtonClickState(true);
+        
     };
 
     const handleStop = () => {
         setIsSpinning(false);
-        clearInterval(intervalRef.current);
-        // navigate("/choosemenu", {})
-        console.log("dd:", getMenu.data.data);
-        for(let i = 0; i < getMenu.data.data.length; i++) {
+        // clearInterval(intervalRef.current);
+        // for(let i = 0; i < getMenu.data.data.length; i++) {
+        //     if(todayLunch === getMenu.data.data[i].name){
+        //         navigate(`/choosemenu?address=${getMenu.data.data[i].address}&todayLunch=${todayLunch}`);
+        //     }
+        // }
+    };
+
+    const changeTest = () => {
+         for(let i = 0; i < getMenu.data.data.length; i++) {
             if(todayLunch === getMenu.data.data[i].name){
-                console.log("제발좀나와라:",getMenu.data.data[i].address);
                 navigate(`/choosemenu?address=${getMenu.data.data[i].address}&todayLunch=${todayLunch}`);
             }
         }
-    };
-
+    }
+    
     const handleSubmit = (event) => {
         event.preventDefault();
+        
     };
 
-
-    const selectLocationHendleClick =() => {
-        setIsOpen(true);
-    }
 
     if(getMenu.isLoading){
         return <div>불러오는 중....</div>
+        
     }
+
 
     if(isSpinning) {
-        intervalRef.current = setInterval(() => {
-            setTodayLunch(slotValue[Math.floor(Math.random() * slotValue.length)]);
-        }, 300);
-    }
+        // intervalRef.current = setInterval(() => {
+        //     setTodayLunch(slotValue[Math.floor(Math.random() * slotValue.length)]);
+        // }, 500);
+        const randomIndex = Math.floor(Math.random() * slotValue.length);
+        setTodayLunch(slotValue[randomIndex]);
+        setIsSpinning(false)
 
+    }
+  
 
 
     return (
         <div css={s.container}>
-            <button css={s.selectButton} onClick={selectLocationHendleClick}>위치 선택하기</button>
             <div css={selectLocation(isOpen)}>
-                <LocalCategory 
-                />
             </div>
             <header css={s.header}>
                 <div css={s.categoryBox}>
@@ -161,11 +146,15 @@ const LunchSelect = () => {
                 <Category />
             </header>
             <main css={s.mainContainer}>
-                <div css={s.selectMenu}>{todayLunch}</div>
+                <div css={s.selectMenu}>
+                    {todayLunch}
+                </div>
                 <form onSubmit={handleSubmit}>
                     {isSpinning 
                     ? (<button css={s.selectButton} type="button" onClick={handleStop}>니 손에 오늘 점심이 달렸다..</button>)
                     : (<button css={s.selectButton} type="button" onClick={handleStart}>점심 무러 갑시다!</button>)}
+
+                    <button onClick={changeTest}>가게 제세히 보기</button>
                 </form>
             </main>
         </div>
--- front_lunch_selecter/src/pages/LunchSelect/category/Category.js
+++ front_lunch_selecter/src/pages/LunchSelect/category/Category.js
@@ -9,39 +9,39 @@ const Category = () => {
 
     return (
         <div css={s.categoryBox}>
-            <label css={s.category} for="">
+            <label css={s.category}>
                 <input css={s.checkbox} type="checkbox"/>
                 경양식
             </label>
-            <label css={s.category} for="">
+            <label css={s.category}>
                 <input css={s.checkbox} type="checkbox"/>
                 고기.구이
             </label>
-            <label css={s.category} for="">
+            <label css={s.category} >
                 <input css={s.checkbox} type="checkbox"/>
                 분식
             </label>
-            <label css={s.category} for="">
+            <label css={s.category}>
                 <input css={s.checkbox} type="checkbox"/>
                 일식
             </label>
-            <label css={s.category} for="">
+            <label css={s.category} >
                 <input css={s.checkbox} type="checkbox"/>
                 중국식
             </label>
-            <label css={s.category} for="">
+            <label css={s.category}>
                 <input css={s.checkbox} type="checkbox"/>
                 치킨
             </label>
-            <label css={s.category} for="">
+            <label css={s.category}>
                 <input css={s.checkbox} type="checkbox"/>
                 패스트푸드
             </label>
-            <label css={s.category} for="">
+            <label css={s.category}>
                 <input css={s.checkbox} type="checkbox"/>
                 한식
             </label>
-            <label css={s.category} for="">
+            <label css={s.category} >
                 <input css={s.checkbox} type="checkbox"/>
                 횟집
             </label>
