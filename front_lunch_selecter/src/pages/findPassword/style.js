51db796c72c9f547651d12fbdb73d0eaed6ff773
dnr1112 <dnr1112@naver.com>
2023-05-22 11:37:00 +0900

    병합

---
+++ front_lunch_selecter/src/pages/findPassword/style.js
@@ -0,0 +1,95 @@
+import { css } from '@emotion/react'
+
+export const container = css`
+    margin-top: 150px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`;
+export const headerContainer = css`
+    margin: auto;
+    display: flex;
+    justify-content: center;
+`;
+export const comment = css`
+    padding-top: 50px;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    
+    font-family: 'Ansungtangmyun-Bold', sans-serif;
+    font-weight: 600;
+    font-size: 50px;
+`;
+export const mainContainer =css`
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`;
+export const input = css`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`;
+<<<<<<< HEAD:front_lunch_selecter/src/pages/findPassword/FindPasswordCss.js
+export const emailLabel = css`
+    padding-top: 50px;
+    padding-bottom: 10px;
+    color: black;
+    font-family: 'Ansungtangmyun-Bold', sans-serif;
+    font-weight: 600;
+    font-size: 40px;
+`;
+export const codeLabel = css`
+    padding-top: 140px;
+    padding-bottom: 10px;
+    color: black;
+    font-family: 'Ansungtangmyun-Bold', sans-serif;
+    font-weight: 600;
+    font-size: 40px;
+`;
+export const blank = css`
+    margin-top: 70px;
+
+=======
+
+export const inputLabel = css`
+    padding-top: 100px;
+    padding-bottom: 10px;
+    color: rgb(221,164,79);
+>>>>>>> main:front_lunch_selecter/src/pages/findPassword/style.js
+`;
+export const footerContainer =css`
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`;
+export const checkButton = css`
+    width: 850px;
+    height: 80px;
+    margin-top: 190px;
+    margin-bottom: 300px;
+    border: none;
+    border-bottom: 1px solid #dbdbdb;
+    background-color: black;
+    color: white;
+    border-radius: 20px;
+    font-family: 'Ansungtangmyun-Bold', sans-serif;
+    font-weight: 600;
+    font-size: 40px;
+    cursor: pointer;
+    &:hover {
+        color: black;
+        border-radius: 20px;
+        border-bottom: 1px solid #fafafa;
+        background-color: #fafafa;
+    }
+    &:active {
+        color: black;
+        border-radius: 20px;
+        border-bottom: 1px solid #dbdbdb;
+        background-color: #dbdbdb;
+    }
+`;
---
+++ lunchSelecter/src/main/java/com/korit/lunchSelect/dto/lunch/RoomReqDto.java
@@ -0,0 +1,9 @@
+package com.korit.lunchSelect.dto.lunch;
+
+import lombok.Data;
+
+@Data
+public class RoomReqDto {
+	private int room_maseter_id;
+	private int flag;
+}
