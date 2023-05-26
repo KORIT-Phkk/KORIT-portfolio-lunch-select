e27b36a5d2ae113bf8b6d2887ea7e88906ec83f1
kimjonghn <nick6566@naver.com>
2023-05-26 17:37:55 +0900

    Fix: 룰렛페이지 디자인중

--- front_lunch_selecter/src/pages/LunchSelect/Roulette/Roulette.js
+++ front_lunch_selecter/src/pages/LunchSelect/Roulette/Roulette.js
@@ -3,125 +3,28 @@ import { css, keyframes } from '@emotion/react';
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
-// import * as s from './style';
+import * as s from './style';
 
-const test2 = css`
-    font-size: 100px;
-`;
 
-const b = css`
-  width: 100%;
-  height: 800px;
-  border: 1px solid #121212;
-  align-items: center;
-  justify-content: center;
-  overflow-y: hidden;
-`;
 
-const a = css`
-  font-size: 100px;
-  margin: 300px auto;
-`;
 
-const test = css`
-  color: #999;
-  text-transform: uppercase;
-  font-size: 36px;
-  font-weight: bold;
-  padding-top: 200px;
-  width: 100%;
-  display: block;
-  margin: auto;
-`;
-
-const rouletteBoxStyles = css`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 80%;
-  height: 500px;
-  border: 1px solid #121212;
-  font-size: 100px;
-  margin-top: 300px;
-  margin-left: 130px;
-  overflow: hidden;
-  position: relative;
-`;
-
-const enterAnimation = keyframes`
-  0% {
-    opacity: 0;
-    transform: translateY(-100%);
-  }
-
-  100% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
-
-const exitAnimation = keyframes`
-  0% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-
-  100% {
-    opacity: 0;
-    transform: translateY(100%);
-  }
-`;
 
 const Roulette = () => {
-    const navigate = useNavigate();
-    const [ stopRoulette, setStopRoulette ] = useState(false);
-
-    const [searchParams, setSearchParams] = useSearchParams();
-    let restaurantNames  = searchParams.get("todayLunch");
-    const restaurantNameList = restaurantNames  ? restaurantNames .split(",") : [];
-
-    const [currentNameIndex, setCurrentNameIndex] = useState(0);
-
-  
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentNameIndex((prevIndex) => (prevIndex + 1) % restaurantNameList.length);
-      }, 100);
-  
-      return () => {
-        clearInterval(interval);
-      };
-    }, [restaurantNameList]);
-  
-const stopRouletteHandleClick = () => {
-    navigate(`/lunchselect/result`);
-}
+ 
 
   return (
-    <div>
-    <main css={test}>
-        <div css={rouletteBoxStyles}>
-          {restaurantNameList.map((restaurantName, index) => (
-            <div
-              key={index}
-              css={css`
-                position: absolute;
-                opacity: ${index === currentNameIndex ? 1 : 0};
-                transition: opacity 0.2s;
-                animation-duration: 1s;
-                animation-fill-mode: both;
-                animation-timing-function: ease-in-out;
-                ${index === currentNameIndex ? enterAnimation : exitAnimation};
-              `}
-            >
-              <p>{restaurantName}</p>
-            </div>
-          ))}
-        </div>
-    </main>
-    <button onClick={stopRouletteHandleClick} css={test2}>멈춰</button>
+    <div css={s.container}>
+      <header css={s.headerContainer}>
+        <img src="../main/logo1.png" css={s.img} />
+      </header>
+      <main css={s.mainContainer}>
+        룰렛
+      </main>
+      <footer css={s.footerContatiner}>
+          <button css={s.detailsButton}>상세히보기</button>
+          <button css={s.returnButton}>다시돌리기</button>
+
+      </footer>
    </div>
 );
 };
--- front_lunch_selecter/src/pages/LunchSelect/Roulette/style.js
+++ front_lunch_selecter/src/pages/LunchSelect/Roulette/style.js
@@ -1,90 +1,65 @@
 import { css } from '@emotion/react';
-import '../../fonts/ansungfont.css';
 
 export const container = css`
     display: flex;
     justify-content: center;
-    flex-direction: column;  
-`;
-
-export const mapExplain = css`
-    display: flex;
-    justify-content: center;
-    /* align-items: center; */
-    margin-top: 30px;
-    font-size: 40px;
-    font-family: 'Ansungtangmyun-Bold', sans-serif;
-`;
-
-export const header = css`
-    margin: 20% auto;
-    display: flex;
-    justify-content: center;
     flex-direction: column;
 `;
-export const categoryBox = css`
+export const headerContainer = css`
     display: flex;
     justify-content: center;
-    margin-top: 100px;
-    margin-bottom: 50px;
 `;
-export const category = css`
-    font-size: 70px;
+export const img = css`
+    width: 270px;
+    height: 196px;
+`;
+export const mainContainer = css`
+    display: flex;
+    justify-content: center;
+`;
+export const footerContatiner = css`
+    display: flex;
+    justify-content: center;
+`;
+export const detailsButton = css`
+    width: 100px;
+    height: 30px;
+    border: none;
+    background-color: black;
+    color: white;
+    border-radius: 5px;
     font-family: 'Ansungtangmyun-Bold', sans-serif;
-`;
-
-export const mainContainer =css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-
-    width: 100%;
-    height: 60%;
-`;
-
-export const footerContainer =css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    width: 100%;
-    height: 20%;
-`;
-
-export const selectMenu = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    margin: 100px;
-
-    height: 50%;
-    width: 100%;
-
-    font-size: 100px;
-`;
-
-export const selectButton = css`
-    border: 1px solid #121212;
-    border-radius: 7px;
-    font-size: 50px;
-
-    margin: 50px;
-
-    background-color: #fafafa;
-
-
-    &:hover{
+    font-weight: 100;
+    font-size: 15px;
+    cursor: pointer;
+    &:hover {
+        color: black;
         background-color: #dbdbdb;
     }
-
-    &:active{
-        background-color: #eee;
+    &:active {
+        color: black;
+        background-color: whitesmoke;
+    }
+`;
+export const returnButton = css`
+    width: 100px;
+    height: 30px;
+    margin-left: 20px;
+    border: none;
+    background-color: black;
+    color: white;
+    border-radius: 5px;
+    font-family: 'Ansungtangmyun-Bold', sans-serif;
+    font-weight: 100;
+    font-size: 15px;
+    cursor: pointer;
+    &:hover {
+        color: black;
+        background-color: #dbdbdb;
+    }
+    &:active {
+        color: black;
+        background-color: whitesmoke;
     }
 `;
 
-export const lunchMenu = css`
-
-    font-size: 100px;
-`;
