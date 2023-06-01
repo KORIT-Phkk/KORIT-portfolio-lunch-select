34567b6f5ae4f80d247f6944ee783e4ee83bb730
dnr1112 <dnr1112@naver.com>
2023-06-01 13:44:25 +0900

    Fix: 룰렛-결과창 오류 수정, 코드 재구성 - 없는 주소 받아 올 시 생기는 오류 해결

--- front_lunch_selecter/src/pages/main/Main.js
+++ front_lunch_selecter/src/pages/main/Main.js
@@ -2,19 +2,16 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { IoMdContact } from 'react-icons/io';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import UserInfo from '../../components/userInfoGroup/UserInfo';
 import * as s from './style';
-import { Reveal, Timeline, Tween } from 'react-gsap'
+import { Reveal, Tween } from 'react-gsap'
 
 const Main = () => {
     const navigate = useNavigate();
     const [ isOpen, setIsOpen ] = useState(false);
     const [ joinCode, setJoinCode ] = useState("");
-    const [imageSrc, setImageSrc] = useState("../main/logo1.png");
-    const [isClicked, setIsClicked] = useState(false);
-    const [ userId, setUserId ] = useState("");
     const [ showElements, setShowElements ] = useState(false);
 
     useEffect(() => {
@@ -73,23 +70,13 @@ const Main = () => {
     const lunchSelectClickHandle = () => {
         createRoom.mutate();
     }
-    
-    const imageHandle = () => {
-        if(isClicked){
-            setImageSrc("../main/logo1.png");
-            setIsClicked(false);
-        }else{
-            setImageSrc("../main/logo2.png");
-            setIsClicked(true);
-        }
-    }
 
     return (
         <div>
             {!showElements && 
                 <Reveal repeat>
                     <Tween from={{ opacity: 0 }} duration={2}>
-                        <h3>Hello</h3>
+                        <h3 css={s.hello}>Hello</h3>
                     </Tween>
                 </Reveal>
             }
@@ -101,18 +88,14 @@ const Main = () => {
                                 <IoMdContact css={s.settingButton} onClick={userInfoHandle} />
                                 <UserInfo css={s.userInfo} isOpen={isOpen}/>
                             </div>
-                            <div css={s.logoContainer}>
-                                <img css={s.logo} onClick={imageHandle} src={imageSrc} alt=""/>
-                            </div>
                         </header>
 
                         <main css={s.mainContainer}>
-                            <div css={s.lunchSelect}>
-                                <button css={s.lunchButton} onClick={lunchSelectClickHandle} >점심</button>
-                            </div>
+                            <img css={s.imgCss} src="../main/logo1.png" alt=""/>
                         </main>
 
                         <footer css={s.footerContainer}>
+                            <button css={s.lunchButton} onClick={lunchSelectClickHandle} >점심</button>
                         </footer>
                     </div>
                 </div>
